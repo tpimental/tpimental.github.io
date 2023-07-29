d3b6b81332aeba644bdab77d73498705bea32fd0
Tyler Pimental <tylerpimental@gmail.com>
2023-07-28 18:28:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+// Fetch the names from the JSON file
+fetch('names.json')
+    .then(response => response.json())
+    .then(names => {
+        // Get the <ul> element to add the names
+        const nameList = document.getElementById('nameList');
 
-let playerX = 50;
-let playerY = canvas.height - 50;
-let playerWidth = 30;
-let playerHeight = 30;
-
-function canvasClick(event) {
-    const rect = canvas.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
-
-    if (
-        mouseX >= playerX &&
-        mouseX <= playerX + playerWidth &&
-        mouseY >= playerY &&
-        mouseY <= playerY + playerHeight
-    ) {
-        playerX = Math.random() * (canvas.width - playerWidth);
-        playerY = Math.random() * (canvas.height - playerHeight);
-    }
-}
-
-canvas.addEventListener('click', canvasClick);
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = 'red';
-    ctx.fillRect(playerX, playerY, playerWidth, playerHeight);
-
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
+        // Loop through the names and create <li> elements for each name
+        names.forEach(name => {
+            const li = document.createElement('li');
+            li.textContent = name;
+            nameList.appendChild(li);
+        });
+    })
+    .catch(error => console.error('Error fetching names:', error));
