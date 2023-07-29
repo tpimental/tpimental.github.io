7e6d44f1a384bfd2edd013023bcec2ce2581afaf
Tyler Pimental <tylerpimental@gmail.com>
2023-07-28 20:25:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// Fetch the names from the JSON file
-fetch('sorted_names.json')
-    .then(response => response.json())
-    .then(names => {
-        // Get the <ul> element to add the names
-        const nameList = document.getElementById('nameList');
+const wordList = document.getElementById('word-list');
+const containerHeight = wordList.clientHeight;
 
-        // Loop through the names and create <li> elements for each name
-        names.forEach(name => {
-            const li = document.createElement('li');
-            li.textContent = name;
-            nameList.appendChild(li);
-        });
-    })
-    .catch(error => console.error('Error fetching names:', error));
+let words = [];
+
+function fetchWords() {
+    fetch('sorted_names.json')
+        .then(response => response.json())
+        .then(data => {
+            words = data;
+            updateView();
+        })
+        .catch(error => console.error('Error fetching words:', error));
+}
+
+function updateView() {
+    const centerIndex = Math.floor(words.length / 2);
+    wordList.innerHTML = '';
+
+    for (let i = 0; i < words.length; i++) {
+        const element = document.createElement('li');
+        element.textContent = words[i];
+        const offset = i - centerIndex;
+        const scale = 1 - Math.abs(offset) / 5; // Display about 10 items, center item with max scale
+        element.style.opacity = scale.toFixed(2);
+        element.style.fontSize = `${scale.toFixed(2)}em`;
+        wordList.appendChild(element);
+    }
+}
+
+window.addEventListener('scroll', updateView);
+window.addEventListener('resize', updateView);
+
+fetchWords(); // Fetch words from JSON file and initialize the view
