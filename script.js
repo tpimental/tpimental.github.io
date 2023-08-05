8d55d20d8d3147c33bc392e94ae748de8e04999e
Tyler Pimental <tylerpimental@gmail.com>
2023-08-04 23:09:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,45 @@ document.addEventListener("DOMContentLoaded", function () {
       data.forEach((item) => {
         const dataItem = document.createElement("div");
         dataItem.classList.add("data-item");
-        dataItem.textContent = item;
+
+        const textContainer = document.createElement("span");
+        textContainer.textContent = item;
+        dataItem.appendChild(textContainer);
+
+        const heartButton = document.createElement("button");
+        heartButton.textContent = "❤";
+        heartButton.classList.add("heart-button");
+        heartButton.addEventListener("click", () => {
+          increaseCounter(item);
+        });
+        dataItem.appendChild(heartButton);
+
         dataContainer.appendChild(dataItem);
       });
     })
     .catch((error) => console.error("Error fetching data:", error));
 });
+
+const counterMap = {};
+
+function increaseCounter(value) {
+  if (counterMap[value]) {
+    counterMap[value]++;
+  } else {
+    counterMap[value] = 1;
+  }
+
+  updateCounters();
+}
+
+function updateCounters() {
+  const dataItems = document.querySelectorAll(".data-item");
+
+  dataItems.forEach((dataItem) => {
+    const textContainer = dataItem.querySelector("span");
+    const value = textContainer.textContent;
+
+    const counter = counterMap[value] || 0;
+    textContainer.textContent = `${value} (${counter} ❤)`;
+  });
+}
