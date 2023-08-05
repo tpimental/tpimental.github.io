1aeea8058c555ac84fabc3445b4f01b2decc69b7
Tyler Pimental <tylerpimental@gmail.com>
2023-08-04 22:52:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-const wordList = document.getElementById('word-list');
-const containerHeight = wordList.clientHeight;
-let words = [];
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("sorted_names.json")
+    .then(response => response.json())
+    .then(data => displayItems(data));
+});
 
-function fetchWords() {
-    fetch('sorted_names.json')
-        .then(response => response.json())
-        .then(data => {
-            words = data;
-            updateView();
-        })
-        .catch(error => console.error('Error fetching words:', error));
+function displayItems(items) {
+  const itemRow = document.getElementById("itemRow");
+
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("col-md-4", "mb-4");
+    itemCard.innerHTML = `
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text">${item.description}</p>
+        </div>
+      </div>
+    `;
+
+    itemRow.appendChild(itemCard);
+  }
 }
-
-function updateView() {
-    const centerIndex = Math.floor(words.length / 2);
-    wordList.innerHTML = '';
-
-    for (let i = 0; i < words.length; i++) {
-        const element = document.createElement('li');
-        element.textContent = words[i];
-        const offset = i - centerIndex;
-        const scale = 1 - Math.abs(offset) / 5; // Display about 10 items, center item with max scale
-        element.style.opacity = scale.toFixed(2);
-        element.style.fontSize = `${scale.toFixed(2)}em`;
-        wordList.appendChild(element);
-    }
-}
-
-window.addEventListener('scroll', updateView);
-window.addEventListener('resize', updateView);
-
-fetchWords(); // Fetch words from JSON file and initialize the view
